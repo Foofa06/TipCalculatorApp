2df18dc2f9c6445a9be359c5017c96644e3544ee
Foofa <fdjass06@gmail.com>
2026-01-27 18:27:17 -0500

    Initial Commit

---
+++ TipCalculatorApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TipCalculatorApp
+//
+//  Created by Fawzia Jassim on 1/27/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TipCalculatorAppTests/TipCalculatorAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TipCalculatorAppTests.swift
+//  TipCalculatorAppTests
+//
+//  Created by Fawzia Jassim on 1/27/26.
+//
+
+import Testing
+
+struct TipCalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
